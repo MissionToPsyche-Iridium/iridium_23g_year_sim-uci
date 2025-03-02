b410d84ce2b567ef8102af8aca655d3877322eb4
qmlim <queensleylim23@gmail.com>
2025-03-01 21:15:51 -0800

    feat: Logos agged on TitleScreen, Most button functionality implemented

--- Assets/Scripts/TitleScreen.cs
+++ Assets/Scripts/TitleScreen.cs
@@ -3,7 +3,19 @@ using UnityEngine.SceneManagement;
 
 public class TitleScreen : MonoBehaviour
 {
-    public void NewJourney() {
+    public void NewMission() {
         SceneManager.LoadSceneAsync("RocketScene");
+    }
+
+    public void ContinueMission() {
+
+    }
+
+    public void OpenCredits() {
+
+    }
+
+    public void ExitGame() {
+        Application.Quit();
     }
 }
