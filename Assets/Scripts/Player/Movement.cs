4aab9daf09af1f9e9c3dc2eb44df6ece1c945da0
danehobrecht <70723095+danehobrecht@users.noreply.github.com>
2025-03-04 11:14:47 -0800

    Added movement relative to camera direction

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -1,112 +1,132 @@
-using System.Collections;
 using UnityEngine;
+using System.Collections;
 using UnityEngine.InputSystem;
 
 public abstract class Movement : MonoBehaviour
 {
-    #region --
-    PlayerInputActions _inputActions;
+	#region --
+	PlayerInputActions _inputActions;
 
-    [SerializeField]
-    protected MoveData moveData;
+	[SerializeField]
+	protected MoveData moveData;
 
-    public Transform planet;
-    [SerializeField]
-    Transform transformBody;
+	public Transform planet;
+	[SerializeField]
+	Transform transformBody;
 
-    protected Vector3 movementVector;
-    protected Vector3 gravityDirection;
-    public float gravityStrength = 1f;
-    protected Vector3 jumpVector;
-    
-    [SerializeField]
-    LayerMask GroundLayerMask;
-    [SerializeField]
-    Transform groundCollider;
+	protected Vector3 movementVector;
+	protected Vector3 gravityDirection;
+	public float gravityStrength = 1f;
+	protected Vector3 jumpVector;
+	
+	[SerializeField]
+	LayerMask GroundLayerMask;
+	[SerializeField]
+	Transform groundCollider;
+	[SerializeField]
+	Transform cameraTransform; // Reference to camera
 
-    RayData groundData;
-    #endregion
 
-    void Start()
-    {
-        groundData = new RayData();
+	RayData groundData;
+	#endregion
 
-        _inputActions = new PlayerInputActions();
-        _inputActions.Enable();
-        _inputActions.PlayerActionmap.Jump.performed += Jump;
-    }
-    
-    void Update()
-    {
-        ApplyGravity();
-        CheckGround();
-        RotateToSurface();
-        Move();
-    }
 
-    void ApplyGravity()
-    {
-        gravityDirection = (planet.position - transform.position).normalized;
+	void Start()
+	{
+		groundData = new RayData();
 
-        if (!groundData.grounded)
-            gravityStrength += planet.GetComponent<Planet>().GravitationalPull * Time.deltaTime;
-        else
-            gravityStrength = moveData.surfaceGravity;
-    }
+		_inputActions = new PlayerInputActions();
+		_inputActions.Enable();
+		_inputActions.PlayerActionmap.Jump.performed += Jump;
+	}
 
-    void RotateToSurface()
-    {
-        Quaternion gravityRotation = Quaternion.FromToRotation(transform.up, -gravityDirection) * transform.rotation;
-        Quaternion surfaceRotation = Quaternion.FromToRotation(transform.up, groundData.normal) * transform.rotation;
-        Quaternion finalRotation = Quaternion.Lerp(gravityRotation, surfaceRotation, moveData.stickToSurface);
-        
-        transform.rotation = Quaternion.Slerp(transform.rotation, finalRotation, moveData.surfaceRotationSpeed * Time.deltaTime);
-    }
 
-    void Jump(InputAction.CallbackContext context)
-    {
-        if(groundData.grounded)
-            StartCoroutine(ApplyJump());
-    }
-    
-    IEnumerator ApplyJump()
-    {
-        gravityStrength = 0f;
-        jumpVector = Vector3.zero;
+	void Update()
+	{
+		ApplyGravity();
+		CheckGround();
+		RotateToSurface();
+		Move();
+	}
 
-        float force = moveData.jumpForce;
-        float t = 0f;
 
-        while(t < moveData.jumpDuration)
-        {
-            jumpVector = -gravityDirection * force;
-            force = Mathf.Lerp(moveData.jumpForce, 0f, t / moveData.jumpDuration);
-            t += Time.deltaTime;
-            yield return new WaitForFixedUpdate();
-        }
+	void ApplyGravity()
+	{
+		gravityDirection = (planet.position - transform.position).normalized;
 
-        jumpVector = Vector3.zero;
-    }
-    
-    void Move()
-    {
-        Vector2 input = _inputActions.PlayerActionmap.Movement.ReadValue<Vector2>();
+		if (!groundData.grounded)
+			gravityStrength += planet.GetComponent<Planet>().GravitationalPull * Time.deltaTime;
+		else
+			gravityStrength = moveData.surfaceGravity;
+	}
 
-        if (groundData.grounded)
-            movementVector = (transformBody.forward * input.y + transformBody.right * input.x) * moveData.moveSpeed;
-    }
-    
-    void CheckGround()
-    {        
-        if(Physics.CheckSphere(groundCollider.position, moveData.groundColSize, GroundLayerMask))
-        {
-            Physics.Raycast(groundCollider.position, -transform.up, out RaycastHit hit, 5f);
-            groundData.grounded = true;
-            groundData.normal = hit.normal;
-            return;
-        }
 
-        groundData.grounded = false;
-        groundData.normal = -gravityDirection;
-    }
+	void RotateToSurface()
+	{
+		Quaternion gravityRotation = Quaternion.FromToRotation(transform.up, -gravityDirection) * transform.rotation;
+		Quaternion surfaceRotation = Quaternion.FromToRotation(transform.up, groundData.normal) * transform.rotation;
+		Quaternion finalRotation = Quaternion.Lerp(gravityRotation, surfaceRotation, moveData.stickToSurface);
+
+		transform.rotation = Quaternion.Slerp(transform.rotation, finalRotation, moveData.surfaceRotationSpeed * Time.deltaTime);
+	}
+
+
+	void Jump(InputAction.CallbackContext context)
+	{
+		if (groundData.grounded)
+			StartCoroutine(ApplyJump());
+	}
+
+	IEnumerator ApplyJump()
+	{
+		gravityStrength = 0f;
+		jumpVector = Vector3.zero;
+
+		float force = moveData.jumpForce;
+		float t = 0f;
+
+		while (t < moveData.jumpDuration)
+		{
+			jumpVector = -gravityDirection * force;
+			force = Mathf.Lerp(moveData.jumpForce, 0f, t / moveData.jumpDuration);
+			t += Time.deltaTime;
+			yield return new WaitForFixedUpdate();
+		}
+
+		jumpVector = Vector3.zero;
+	}
+
+
+	void Move()
+	{
+		Vector2 input = _inputActions.PlayerActionmap.Movement.ReadValue<Vector2>();
+
+		if (groundData.grounded) {
+			//movementVector = (transformBody.forward * input.y + transformBody.right * input.x) * moveData.moveSpeed;
+
+			Vector3 camForward = cameraTransform.forward;
+			Vector3 camRight = cameraTransform.right;
+
+			// Project camera directions onto the movement plane
+			camForward = Vector3.ProjectOnPlane(camForward, groundData.normal).normalized;
+			camRight = Vector3.ProjectOnPlane(camRight, groundData.normal).normalized;
+
+			movementVector = (camForward * input.y + camRight * input.x) * moveData.moveSpeed;
+		}
+	}
+
+
+	void CheckGround()
+	{        
+		if (Physics.CheckSphere(groundCollider.position, moveData.groundColSize, GroundLayerMask))
+		{
+			Physics.Raycast(groundCollider.position, -transform.up, out RaycastHit hit, 5f);
+			groundData.grounded = true;
+			groundData.normal = hit.normal;
+			return;
+		}
+
+		groundData.grounded = false;
+		groundData.normal = -gravityDirection;
+	}
 }
