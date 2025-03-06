d0fc2d02c28611396a4304d21a8ad88572cf9afc
qmlim <queensleylim23@gmail.com>
2025-03-06 12:54:11 -0800

    feat: Enhanced celestial models in SolarSystemView, Sun model bloom effect, imported Space Mono font, minor main UI change with animations

--- Assets/Scripts/UIBehaviour.cs
+++ Assets/Scripts/UIBehaviour.cs
@@ -4,11 +4,15 @@ using UnityEngine.SceneManagement;
 public class UIBehaviour : MonoBehaviour
 {
     public GameObject solarSystemButton;
+    // [SerializeField] private Camera mainCamera;
+    // [SerializeField] private Camera solarCamera;
+    // private bool viewSolarSystem = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // mainCamera.enabled = true;
+        // solarCamera.enabled = false;
     }
 
     // Update is called once per frame
@@ -17,7 +21,16 @@ public class UIBehaviour : MonoBehaviour
         
     }
 
-    public void toSolarSystemView() {
+    public void viewSolarSystemView() {
+        // viewSolarSystem = true;
+        // mainCamera.enabled = false;
+        // solarCamera.enabled = viewSolarSystem;
        SceneManager.LoadSceneAsync("SolarSystemViewScene");
+    }
+
+    public void viewPsycheWorld() {
+        // viewSolarSystem = false;
+        // solarCamera.enabled = viewSolarSystem;
+        // mainCamera.enabled = true;
     }
 }
