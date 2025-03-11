f83177ff8f409fef965275bf1b6ef42b9a37e689
Ivan Quon <ivanson2300@gmail.com>
2025-03-11 00:33:57 -0700

    Integrated movement into PsycheScene

--- Assets/Scripts/Former/CameraBehavior.cs
+++ Assets/Scripts/Former/CameraBehavior.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 public class CameraBehavior : MonoBehaviour {
 	// Start is called once before the first execution of Update after the MonoBehaviour is created
 	void Start() {
-		Cursor.visible = false;
-		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = true;
+		Cursor.lockState = CursorLockMode.None;
 	}
 
 	// Update is called once per frame
