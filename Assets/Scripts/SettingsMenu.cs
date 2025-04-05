0df87f4af6c70e3cf8ee0e5a6590720583377c35
danehobrecht <70723095+danehobrecht@users.noreply.github.com>
2025-04-04 17:09:26 -0700

    Changed CameraBehavior to more appropriate CursorManager

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -2,49 +2,40 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SettingsMenu : MonoBehaviour {
-    public GameObject settingsMenu;
-    public CameraBehavior cameraBehavior;
-    public bool isPaused;
+	public GameObject settingsMenu;
+	public CursorManager cursorManager;
+	public bool isPaused;
 
-    void Start() { settingsMenu.SetActive(false); }
+	void Start() { settingsMenu.SetActive(false); }
 
-    void Update() {
-        if (Input.GetKeyDown(KeyCode.Escape)) {
-            if (isPaused) {
-                ResumeGame();
-            } else {
-                PauseGame();
-            }
-        }
-        if (Time.timeScale == 0f) {
-			cameraBehavior.ToggleCursor(true);
-			cameraBehavior.DisableCursorListeners();
-		} else {
-			cameraBehavior.ToggleCursor(false);
-			cameraBehavior.DisableCursorListeners();
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			if (isPaused) {
+				ResumeGame();
+			} else { PauseGame(); }
 		}
-    }
+	}
 
-    public void PauseGame() {
-        settingsMenu.SetActive(true);
-        Time.timeScale = 0f;
-        isPaused = true;
+	public void PauseGame() {
+		settingsMenu.SetActive(true);
+		Time.timeScale = 0f;
+		isPaused = true;
 
-		cameraBehavior.ToggleCursor(true);
-        cameraBehavior.DisableCursorListeners();
-    }
+		cursorManager.ToggleCursor(true);
+		cursorManager.DisableCursorListeners();
+	}
 
-    public void ResumeGame() {
-        settingsMenu.SetActive(false);
-        Time.timeScale = 1f;
-        isPaused = false;
+	public void ResumeGame() {
+		settingsMenu.SetActive(false);
+		Time.timeScale = 1f;
+		isPaused = false;
 
-		cameraBehavior.ToggleCursor(false);
-        cameraBehavior.EnableCursorListeners();
-    }
+		cursorManager.ToggleCursor(false);
+		cursorManager.EnableCursorListeners();
+	}
 
-    public void GoToMainMenu() {
-        Time.timeScale = 1f;
-        SceneManager.LoadScene("TitleScreen");
-    }
+	public void GoToMainMenu() {
+		Time.timeScale = 1f;
+		SceneManager.LoadScene("TitleScreen");
+	}
 }
