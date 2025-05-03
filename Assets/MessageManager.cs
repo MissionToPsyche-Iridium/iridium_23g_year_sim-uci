dec3be090d327a6c7374b25f93be2bc4ff583f9f
Ivan Quon <70609060+ivanquon@users.noreply.github.com>
2025-05-03 04:18:53 -0700

    Basic Dialogue Manager

---
+++ Assets/MessageManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class MessageManager : MonoBehaviour
+{
+    [SerializeField] public TextMeshProUGUI text;
+    public string[] lines;
+    public float textSpeed;
+    private int index;
+
+    public void Start()
+    {
+        text.text= string.Empty;
+        StartDialogue();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (text.text == lines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                text.text = lines[index];
+            }
+        }
+    }
+
+    void StartDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            text.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void NextLine()
+    {
+        if (index < lines.Length - 1) 
+        {
+            index++;
+            text.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
