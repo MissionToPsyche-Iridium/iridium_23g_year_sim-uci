e11d338b5799efd73094d79b3c72d6a8a6f7d6f4
qmlim <queensleylim23@gmail.com>
2025-05-13 17:46:40 -0700

    feat: Added End Game warning to settings

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -1,12 +1,16 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class SettingsMenu : MonoBehaviour {
 	public GameObject settingsMenu;
+	public GameObject warningPopUp;
 	public CursorManager cursorManager;
 	//public GameObject playerInputActions; // Potential fix for crash upon clicking "Return To Main Menu" in settings?
 	public Scene currentScene;
 	public bool isPaused;
+	public GameObject cancelButton;
 
 	// Start is called before the first frame update
 	void Start() {
@@ -41,10 +45,37 @@ public class SettingsMenu : MonoBehaviour {
 		}
 	}
 
+	public void WarningPopUpOpen() {
+		warningPopUp.SetActive(true);
+		cursorManager.ToggleMenuCursor(true);
+
+        // Force Button to reset visually and logically
+        ResetButtonVisual(cancelButton);
+	}
+
+	public void WarningPopUpClose() {
+		ResetButtonVisual(cancelButton);
+        warningPopUp.SetActive(false);
+	}
+
 	public void GoToMainMenu() {
 		Time.timeScale = 1f;
 		//playerInputActions.PlayerActionmap.Disable(); // (See variable assignment)
 		SceneManager.LoadScene("TitleScreen");
 		cursorManager.ToggleMenuCursor(true);
 	}
+
+	private void ResetButtonVisual(GameObject buttonObject) {
+		var button = buttonObject.GetComponent<Button>();
+		if (button == null) return;
+
+		Animator animator = button.GetComponent<Animator>();
+		if (animator != null)
+		{
+			animator.Play("Normal", 0, 0);
+			button.targetGraphic.color = Color.white;
+			button.transform.localScale = Vector3.one;
+		}
+}
+
 }
