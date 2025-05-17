c2ef5c8b1b125bb88612fb59cf1634a97a0f27c4
danehobrecht <70723095+danehobrecht@users.noreply.github.com>
2025-05-16 18:14:10 -0700

    Cleaned up Dialogue system spacing for readability

--- Assets/Scripts/DialogueSystem/Dialogue.cs
+++ Assets/Scripts/DialogueSystem/Dialogue.cs
@@ -3,17 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Dialogue", menuName = "Scriptable Objects/Dialogue")]
-public class Dialogue : ScriptableObject
-{
 
-    [SerializeField]
-    public List<dialogueType> dialogues;
+public class Dialogue : ScriptableObject {
+	[SerializeField]
+	public List<dialogueType> dialogues;
 
-    [System.Serializable]
-    public class dialogueType {
-        [SerializeField]
-        public string Speaker;
-        [SerializeField]
-        public string Line;
-    }
+	[System.Serializable]
+	public class dialogueType {
+		[SerializeField]
+		public string Speaker;
+		[SerializeField]
+		public string Line;
+	}
 }
